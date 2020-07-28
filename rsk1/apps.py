5e7988f4571bb727db4ae711c0a26b1d486d36ac
unknown <karthikeyan1296@yahoo.com>
2020-07-28 11:59:43 +0530

    templates inheritance

---
+++ rsk1/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Rsk1Config(AppConfig):
+    name = 'rsk1'
---
+++ rsk1/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from rsk1  import views
+app_name="rsk1" #is used to create a namespace(called url mapping)
+
+urlpatterns = [
+    path('',views.index,name="index"),
+    path('home',views.home,name="home"),
+    path('fact/<n>',views.fact,name="fact"),
+    path('child',views.child,name="child")
+    #path("secondary suffix",address of function,name of mapping)
+]
